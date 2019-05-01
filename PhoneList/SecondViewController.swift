2a13ede2a9396bf0e3be1f0f91009616a7d5bdf9
Rob Napier <rob@neverwood.org>
2019-05-01 11:14:44 -0400

    Initial Commit

---
+++ PhoneList/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  PhoneList
+//
+//  Created by Rob Napier on 5/1/19.
+//  Copyright © 2019 Rob Napier. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
